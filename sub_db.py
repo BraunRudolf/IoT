d38da38030368cea7df90b753777187fb6c15eeb
Rudolf Braun <braun.ru@outlook.de>
2021-11-06 10:56:10 +0100

    new files

---
+++ sub_db.py
@@ -0,0 +1,40 @@
+import paho.mqtt.client as mqtt
+from sql_SUB import sensor_Data_Handler
+
+# MQTT Settings 
+MQTT_Broker = "localhost"
+MQTT_Port = 1883
+Keep_Alive_Interval = 45
+MQTT_Topic = "home/office/#"
+
+#Subscribe to all Sensors at Base Topic
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+
+    client.subscribe("home/office/temperature/#")
+
+#Save Data into DB Table
+def on_message(mosq, obj, msg):
+	# This is the Master Call for saving MQTT Data into DB
+	# For details of "sensor_Data_Handler" function please refer "sensor_data_to_db.py"
+	print("MQTT Data Received...")
+	#print( "MQTT Topic: " + msg.topic  )
+	#print( "Data: " + str(msg.payload))
+	sensor_Data_Handler(msg.topic, msg.payload)
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    pass
+
+client = mqtt.Client()
+
+# Assign event callbacks
+client.on_message = on_message
+client.on_connect = on_connect
+client.on_subscribe = on_subscribe
+client.username_pw_set('admin','nnerbv84')
+
+# Connect
+client.connect("localhost", 1883, 60)
+
+# Continue the network loop
+client.loop_forever()
---
+++ sub_db_functions.py
@@ -0,0 +1,53 @@
+import json
+import sqlite3
+
+# SQLite DB Name
+DB_Name =  "IoT.db"
+
+#===============================================================
+# Database Manager Class
+
+class DatabaseManager():
+	def __init__(self):
+		self.conn = sqlite3.connect(DB_Name)
+		self.conn.execute('pragma foreign_keys = on')
+		self.conn.commit()
+		self.cur = self.conn.cursor()
+		
+	def add_del_update_db_record(self, sql_query, args=()):
+		self.cur.execute(sql_query, args)
+		self.conn.commit()
+		return
+
+	def __del__(self):
+		self.cur.close()
+		self.conn.close()
+
+#===============================================================
+# Functions to push Sensor Data into Database
+
+# Function to save Temperature to DB Table
+def DHT22_Temp_Data_Handler(jsonData):
+	#Parse Data 
+	json_Dict = json.loads(jsonData)
+	SensorID = json_Dict['Sensor_ID']
+	Data_n_Time = json_Dict['Timestamp']
+	Temperature = json_Dict['Temperature']
+	Location = json_Dict['Topic']
+	
+	#Push into DB Table
+	dbObj = DatabaseManager()
+	dbObj.add_del_update_db_record("insert into DHT22_Temperature_Data (SensorID, Date_n_Time, Temperature, Location, Location_2, Kind, Kind_2) values (?,?,?,?,?,?,?)",[SensorID, Data_n_Time, Temperature, Location, Location_2, Kind, Kind_2])
+	del dbObj
+	print("Inserted Temperature Data into Database.")
+
+
+#===============================================================
+# Master Function to Select DB Function based on MQTT Topic
+
+def sensor_Data_Handler(Topic, jsonData):
+	if "home/office/temperature/" in Topic: 
+		DHT22_Temp_Data_Handler(jsonData)
+		print('Data saved, ' + Topic)
+
+#===============================================================
