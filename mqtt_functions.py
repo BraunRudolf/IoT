a119ae5e21a1715c496ffa48eb9c85f91e8e8936
Rudolf Braun <braun.ru@outlook.de>
2021-11-14 13:19:56 +0100

    changes

--- mqtt_functions.py
+++ mqtt_functions.py
@@ -8,14 +8,14 @@ def on_pub_connect(client, userdata, flags, rc):
 
 
 # Subscribe to all Sensors at Base Topic
-def on_sub_connect(client, userdata, flags, rc, MQTT_Topic):
+def on_sub_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
     client.subscribe(MQTT_Topic)
 
 
 # Save Data into DB Table
-def on_sub_message(mosq, obj,DB_Name, msg):
+def on_sub_message(mosq, obj, msg):
 	# This is the Master Call for saving MQTT Data into DB
 	# For details of "sensor_Data_Handler" function please refer "sensor_data_to_db.py"
 	sensor_Data_Handler(DB_Name, msg.topic, msg.payload)
